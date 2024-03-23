fdeb5da55048917b1318cdf2f41b4fd8530a506e
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-03-23 11:55:36 -0400

    JDBC

--- src/main/java/org/example/Database2.java
+++ src/main/java/org/example/Database2.java
@@ -5,12 +5,15 @@ import java.util.*;
 import java.sql.*;
 public class Database2 {
     public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement stm = null;
+
         try {
             Class.forName("org.sqlite.JDBC");
 
-            Connection con = DriverManager.getConnection("jdbc:sqlite://Users//SandeepGollapudi//Downloads//sqlite-tools-osx-x64-3450200//univ.bd");
+            con = DriverManager.getConnection("jdbc:sqlite://Users//SandeepGollapudi//Downloads//sqlite-tools-osx-x64-3450200//univ.bd");
 
-            PreparedStatement stm = con.prepareStatement("select * from students where deptno=?");
+            stm = con.prepareStatement("select * from students where deptno=?");
 
             Scanner sc = new Scanner(System.in);
 
@@ -27,13 +30,22 @@ public class Database2 {
                 System.out.print(rs.getString("city") + " ");
                 System.out.println(rs.getInt(4));
             }
-
-            stm.close();
-            con.close();
         } catch (ClassNotFoundException e) {
-            e.getMessage(); // Handle ClassNotFoundException
+            e.printStackTrace();
         } catch (SQLException e) {
-            e.getMessage(); // Handle SQLException
+            e.printStackTrace();
+        } finally {
+            try {
+                stm.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
 }
